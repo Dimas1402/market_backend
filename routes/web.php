04917fd58bd607e4d3bf49278ba67fad432ea381
Dimas1402 <anugerahdimas469@gmail.com>
2021-10-05 17:19:47 +0700

    create product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ DashboardController::class, 'index']);
+Route::get('/', [ DashboardController::class, 'index'])->name('dashboard');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('products',  ProductController::class);
